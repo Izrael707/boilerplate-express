27192c84af5e0bedf2fbe3a2294e09bfaa9fb2f5
Ikechukwu Israel <dkingisreal@gmail.com>
2023-11-22 17:16:44 +0100

    edited myApp file to send html as server response

--- myApp.js
+++ myApp.js
@@ -1,8 +1,15 @@
 let express = require("express");
 let app = express();
 
+// LESSON ONE
+// app.get("/", function (req, res) {
+// 	res.send("Hello Express");
+// });
+
+// LESSON TWO
 app.get("/", function (req, res) {
-	res.send("Hello Express");
+	filePath = __dirname + "/views/index.html";
+	res.sendFile(filePath);
 });
 
 module.exports = app;
